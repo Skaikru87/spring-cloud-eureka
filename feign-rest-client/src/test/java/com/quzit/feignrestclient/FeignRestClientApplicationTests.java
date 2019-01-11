2258c6ce0f2a3b2cc095e393972059ca3c2a0907
mmichalski <mmichalski@unitdoseone.com>
2019-01-11 15:28:32 +0100

    commit all files from feing client

---
+++ feign-rest-client/src/test/java/com/quzit/feignrestclient/FeignRestClientApplicationTests.java
@@ -0,0 +1,17 @@
+package com.quzit.feignrestclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeignRestClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
