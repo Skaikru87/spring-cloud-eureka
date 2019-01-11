0f1809765279c6c8bb15b1bc22a6c63d65849331
mmichalski <mmichalski@unitdoseone.com>
2019-01-11 15:46:32 +0100

    refactoring

--- feign-rest-client/src/main/java/com/quzit/feignrestclient/feignclient/GreetingClient.java
+++ feign-rest-client/src/main/java/com/quzit/feignrestclient/feignclient/GreetingClient.java
@@ -1,4 +1,4 @@
-package com.quzit.feignrestclient;
+package com.quzit.feignrestclient.feignclient;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
