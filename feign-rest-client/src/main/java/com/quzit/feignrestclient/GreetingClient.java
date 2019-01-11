2b55d1dd26489fd7f7fa7c89f9ff6e7093931e20
mmichalski <mmichalski@unitdoseone.com>
2019-01-11 15:27:53 +0100

    second commit feign client

--- feign-rest-client/src/main/java/com/quzit/feignrestclient/GreetingClient.java
+++ feign-rest-client/src/main/java/com/quzit/feignrestclient/GreetingClient.java
@@ -3,7 +3,7 @@ package com.quzit.feignrestclient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("rest-client-api")
+@FeignClient("rest-client-api-1")
 public interface GreetingClient {
     @RequestMapping("/greeting")
     String greating();
